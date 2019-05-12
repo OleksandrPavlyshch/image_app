7f5a10cfa0b484993a8f476b635c75ae42b23425
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2019-05-12 16:45:08 +0300

    add image grid

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,16 +1,33 @@
 <template>
-    <div class="image-gallery">
+    <div>
         <div
              v-if="isLoggedIn"
             class="image-gallery"
         >
             <h1>Image gallery</h1>
-            <img 
-                v-for="image in images" 
-                :key="image.id"
-                :src="image.link" 
-                :alt="image.title"
-            >
+            <div class="image_grid">
+                <div
+                    class="image_grid-item"
+                    v-for="image in images" 
+                    :key="image.id"
+                    :style="{
+                        width: `${image.width*270/image.height}px`,
+                        flexGrow: `${image.width*270/image.height}`
+                    }"
+                >
+                    <i 
+                        :style="{
+                            paddingBottom: `${image.height/image.width*100}%`,
+                        }"
+                    ></i>
+                    <img 
+                        :src="image.link" 
+                        :alt="image.title"
+                    >
+                </div>
+                
+            </div>
+           
         </div>
         <h1 v-else>Please login</h1>
      
@@ -31,3 +48,41 @@ export default {
 
 }
 </script>
+
+<style lang="scss" scoped>
+.image_grid {
+    display: flex;
+    flex-wrap: wrap;
+    &::after {
+        content: '';
+        flex-grow: 999999999;
+    }
+    &-item {
+        margin: 2px;
+        background-color: violet;
+        position: relative;
+        i {
+            display: block;
+        }
+        img {
+            position: absolute;
+            top: 0;
+            width: 100%;
+            vertical-align: bottom;
+        }
+    }
+}
+
+// .image-grid {
+//     display: grid;
+//     // column-count: 3;
+//     grid-template-columns: 1fr 1fr 1fr;
+//     grid-gap: 10px;
+//     grid-template-columns: repeat(auto-fill, minmax(330px,1fr));
+//     grid-auto-rows: 40px;
+//     img {
+//         max-width: 100%;
+//     }
+// }
+</style>
+
